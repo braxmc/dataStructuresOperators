1b6290131c121f75ec7823396056e5cb366112e2
Braxton McClellan <thebraxmc@gmail.com>
2021-03-14 23:43:42 -0600

    coding challange

--- script.js
+++ script.js
@@ -245,43 +245,6 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
-/*
-// 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
-
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
-
-// 3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-
-// 4.
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
-
-// 5.
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
-
-// 6.
-const printGoals = function (...players) {
-  console.log(players);
-  console.log(`${players.length} goals were scored`);
-};
-
-// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-// printGoals('Davies', 'Muller');
-printGoals(...game.scored);
-
-// 7.
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
-*/
-
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -322,3 +285,40 @@ const game = {
     team2: 6.5,
   },
 };
+
+// 1. 
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+console.log(players1Final);
+
+// 5.
+// const team1 = game.odds.team1;
+// console.log(team1);
+
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+
+// 6.
+const printGoals = function(...players) {
+  console.log(`${players.length} goals were scored`);
+  console.log(...players);
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski');
+printGoals('Brax', 'Mom');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
