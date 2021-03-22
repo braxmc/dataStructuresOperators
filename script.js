822ea300446eaaee0c20128ee880a8e1d86271c9
Braxton McClellan <thebraxmc@gmail.com>
2021-03-21 23:32:05 -0600

    working with string pt4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
@@ -52,6 +52,21 @@ const restaurant = {
     console.log(`The main ingredient is ${mainIngredient}, the other ingredient(s) are ${otherIngredients}`);
   }
 };
+
+// ---------------------------------------------------------
+
+// // WORKING WITH STRINGS Part 4
+
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const destination = str => str.slice(0, 3).toUpperCase();
+
+for(const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';')
+  const output = `${type.replace(/_/g, ' ').trim()} from ${destination(from)} to ${destination(to)} (${time.replace(':', 'h')})`.padStart(45);
+  console.log(output);
+}
+
 
 // ---------------------------------------------------------
 
@@ -644,20 +659,20 @@ const game = {
 // delayed_departure
 
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
 
-document.querySelector('button').addEventListener('click', function() {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n');
+// document.querySelector('button').addEventListener('click', function() {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
 
-  for(const [i, row] of rows.entries()) {
-    const [first, second] = row.trim().toLowerCase().split('_')
-    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
-    console.log(`${output.padEnd(20)} ${'#'.repeat(i + 1)}`);
-  }
-})
+//   for(const [i, row] of rows.entries()) {
+//     const [first, second] = row.trim().toLowerCase().split('_')
+//     const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+//     console.log(`${output.padEnd(20)} ${'#'.repeat(i + 1)}`);
+//   }
+// })
 
 // // CODING CHALLENGE 3
 
